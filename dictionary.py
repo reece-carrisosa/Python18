a65e6239101d01d1444d2409763ac453cd80f416
Reece Carrisosa <r.carrisosa@yahoo.com>
2018-01-23 13:17:43 -0800

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,3 @@
+aboutMe = {"My name": "is Reece", "My age": "is 27", "My Country of Birth": "is the U.S.A", "My favorite Language": "is Spanish"}
+for key,data in aboutMe.iteritems():
+    print key,data
